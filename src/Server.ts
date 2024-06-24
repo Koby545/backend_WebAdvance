b32f867e4d97fb7d232fff7476ee1c5cb56b38a0
Koby545 <122885890+Koby545@users.noreply.github.com>
2024-06-24 14:22:47 +0300

    yaakov+

--- src/Server.ts
+++ src/Server.ts
@@ -22,5 +22,6 @@ init().then(app =>{
         
     app.listen(process.env.PORT, () => {
         console.log(`Example app listening at http://localhost:${process.env.PORT}`);
+        console.log(`yaakov`);
     });
 });
